4a4fb783bf1da688e3a1321410fbb268d017da54
jstnf <mcpokeball@gmail.com>
2019-04-03 06:30:47 -0700

    Formatting and backwards compat

--- src/com/jstnf/pi/Window.java
+++ src/com/jstnf/pi/Window.java
@@ -1,26 +1,23 @@
 package com.jstnf.pi;
 
-import java.awt.Canvas;
-import java.awt.Dimension;
+import javax.swing.*;
+import java.awt.*;
 
-import javax.swing.JFrame;
+public class Window extends Canvas
+{
+	public Window(int width, int height, String title, Engine physEngine)
+	{
+		JFrame frame = new JFrame(title);
 
-public class Window extends Canvas {
+		frame.setPreferredSize(new Dimension(width, height));
+		frame.setMaximumSize(new Dimension(width, height));
+		frame.setMinimumSize(new Dimension(width, height));
 
-    private static final long serialVersionUID = 7857328500628797687L;
-
-    public Window (int width, int height, String title, Engine physEngine) {
-        JFrame frame = new JFrame(title);
-
-        frame.setPreferredSize(new Dimension(width, height));
-        frame.setMaximumSize(new Dimension(width, height));
-        frame.setMinimumSize(new Dimension(width, height));
-
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
-        frame.add(physEngine);
-        frame.setVisible(true);
-        physEngine.start();
-    }
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.add(physEngine);
+		frame.setVisible(true);
+		physEngine.start();
+	}
 }
